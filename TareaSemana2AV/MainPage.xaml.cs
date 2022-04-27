97c3e9af7073e256bfade48f453d989d6dcf66a9
ANAI <ANAI@DESKTOP-FOQ2ME0>
2022-04-27 18:28:05 -0500

    Agregar archivos de proyecto.

---
+++ TareaSemana2AV/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TareaSemana2AV
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnLogin_Clicked(object sender, EventArgs e)
+        {
+            String user = txtUser.Text;
+            String pass = txtPass.Text;
+
+            if ((user == "estudiante2021") && (pass == "uisrael2021")){
+                await Navigation.PushAsync(new Tarea_Semana2(user));
+            }
+            else
+            {
+                await DisplayAlert("Mensaje de Alerta","El usuario o password son incorrectos","Cerrar");
+            }
+           
+        }
+    }
+}
---
+++ TareaSemana2AV/Tarea_Semana2.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TareaSemana2AV
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Tarea_Semana2 : ContentPage
+    {
+        public Tarea_Semana2(String User)
+        {
+            InitializeComponent();
+            lblUser.Text = User;
+        }
+
+        private void btnCalcular_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                double nota1 = Convert.ToDouble(txtNota1.Text);
+                double nota2 = Convert.ToDouble(txtNota2.Text);
+                double examen1 = Convert.ToDouble(txtExamen1.Text);
+                double examen2= Convert.ToDouble(txtExamen2.Text);
+
+                double parcial1 = (nota1 * 0.3) + (examen1 * 0.2);
+                double parcial2 = (nota2 * 0.3) + (examen2 * 0.2);
+
+                txtParcial1.Text = parcial1.ToString();
+                txtParcial2.Text = parcial2.ToString();
+
+                double notafinal = parcial1 + parcial2;
+
+                txtNotaFinal.Text = notafinal.ToString();
+
+                if (Convert.ToDouble(txtNotaFinal.Text) >= 7)
+                {
+                    DisplayAlert("Mensaje de alerta", "Aprobado", "Cerrar");
+                }
+
+                if (Convert.ToDouble(txtNotaFinal.Text) >= 5 && Convert.ToDouble(txtNotaFinal.Text) <= 6.9)
+                {
+                    DisplayAlert("Mensaje de alerta", "Complementario", "Cerrar");
+                }
+                if (Convert.ToDouble(txtNotaFinal.Text) <5)
+                {
+                    DisplayAlert("Mensaje de alerta", "Reprobado", "Cerrar");
+                }
+            }
+            catch(Exception ex)
+            {
+
+                DisplayAlert("ERROR", ex.Message, "Cerrar");
+
+            }
+        }
+    }
+}
